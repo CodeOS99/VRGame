51eddaf4f5367a6be896f5aeae1ef9bde3eaaacc
Kushagra Sharma <kush.kuhu@gmail.com>
2025-08-24 20:24:14 +0530

    made quit button work

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,3 +2,6 @@ extends CanvasLayer
 
 func _on_play_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_game.tscn")
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().quit()
